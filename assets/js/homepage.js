c965a4c91bb06ed4e5ceeec476d1185b123b2113
Joce <jocelynn4355@gmail.com>
2023-07-17 21:08:46 -0500

    added changes to html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,21 +1,20 @@
 var userFormEl = document.querySelector('#user-form');
 var apiKey = '0410adc31dc23c4ff294045bbe91f64b09e4510119daf47a5f3eb24e675f3ba3';
 var apiUrl = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR';
-var choosenCoinName = document.querySelector('#cryptoName');
+// var choosenCoinName = document.querySelector('#cryptoName');
 var chosenCoinSelect = document.querySelector('#cryptoSelect');
-var searchBtn = document.querySelector('#searchBtn');
 var coin_list = 'https://www.cryptocompare.com/coins/list/all/USD/1';
 var top_coins_key = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD&api_key=' + apiKey;
 var single_symbol_url = 'https://min-api.cryptocompare.com/data/price?&tsyms=USD&api_key=' + apiKey;
 var coin_news_key = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=' + apiKey;
 var coin_news = document.querySelector('#coin-price-results');
-var div_list = document.querySelector('#crypto-container');
+var div_list = document.querySelector('.crypto-container');
 
 // Creating a ordered list element
 var ol_list = document.createElement("ol");
 
 // Creating List Items
-var li1 = document.createElement("li");
+var li1 = document.createElement("ol");
 
 li1.textContent = "Testing list";
 
@@ -54,7 +53,7 @@ function top_coin_call() {
                     var i = 0;
                     i < data.Data.length;
                     i++) {
-                    var price_list = document.createElement('li');
+                    var price_list = document.createElement('ol');
                     price_list.textContent = data.Data[i].CoinInfo.FullName + " " + data.Data[i].DISPLAY.USD.PRICE;
                     coin_news.appendChild(price_list);
 
@@ -122,20 +121,21 @@ function coin_news_call() {
 //     })
 
 // addEventListener.searchBtn('click') ON CLICK callCoinInfo function
-function callCoinInfo() {
+// function callCoinInfo() {
 
-}
+// }
 
-searchBtn.addEventListener('click', () => {
+// searchBtn.addEventListener('click', () => {
     
-});
+// });
 
-var formSubmit = function (event) {
-    event.preventDefault();
-    fetchSingleToken(chosenCoinSelect.value);
-}
+// var formSubmit = function (event) {
+//     event.preventDefault();
+//     fetchSingleToken(chosenCoinSelect.value);
+// }
 
-userFormEl.addEventListener('submit', formSubmit);
+// userFormEl.addEventListener('submit', formSubmit);
 // top_coin_call();
+fetchSingleToken();
 top_coin_call();
 coin_news_call();
